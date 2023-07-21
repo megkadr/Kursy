a7b8259ef9beed31adb35baeffa5d54e1dc65525
Artur Rezmer <a.rezmer@sellpander.pl>
2023-07-21 14:00:42 +0200

    Function lesson( aliases, never type, literal types)

--- Typescript/build/js/main.js
+++ Typescript/build/js/main.js
@@ -2,18 +2,52 @@
 let stringArr = ['one', 'hey', 'Artur'];
 let guitars = ['strat', 'Les Paul', 5150];
 let mixedData = ['EVH', 1084, true];
-let myArr = [];
-const exampleObj = {
-    prop1: 'Dave',
-    ptop2: true,
-};
-exampleObj.prop1 = 'Artur';
 let JP = {
     name: 'Jan',
     active: false,
     albums: ['yes', 'no']
 };
-const greetGuitarist = (guitarist) => {
-    return `Hello ${guitarist.name}!`;
+// Literal types
+let myName;
+let userName;
+userName = 'Amy';
+// functions
+const add = (a, b) => {
+    return a + b;
 };
-console.log(greetGuitarist(JP));
+const logMsg = (message) => {
+    console.log(message);
+};
+logMsg('Hello !');
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
+};
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+// default param value
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+// Rest Parameters
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2, 3, 4));
+// custom type guard
+const isNumber = (value) => {
+    return typeof value === 'number'
+        ? true : false;
+};
--- Typescript/src/main.ts
+++ Typescript/src/main.ts
@@ -3,21 +3,15 @@ let stringArr = ['one','hey','Artur']
 let guitars =['strat', 'Les Paul', 5150]
 
 let mixedData = ['EVH', 1084, true]
+//Aliases
+type stringOrNumber = string | number
 
-let myArr: string[] = []
-
-
-const exampleObj = {
-    prop1: 'Dave',
-    ptop2: true,
-}
-
-exampleObj.prop1 = 'Artur'
+type stringOrNumberArray = (string | number)[]
 
 interface Guitarist  {
-    name: string,
-    active?: boolean,
-    albums: (string | number)[]
+    name?: string,
+    active: boolean,
+    albums: stringOrNumberArray
 }
 
 let JP: Guitarist = {
@@ -26,8 +20,69 @@ let JP: Guitarist = {
     albums: ['yes','no']
 }
 
-const greetGuitarist = (guitarist: Guitarist) => {
-    return `Hello ${guitarist.name}!`
+type UserId = stringOrNumber
+
+// Literal types
+
+let myName: 'Artur'
+
+let userName: 'Dave' | 'Artur' | 'Amy'
+userName = 'Amy'
+
+// functions
+
+const add = (a: number,b: number): number => {
+    return a + b
 }
 
-console.log(greetGuitarist(JP));
+const logMsg = (message: any): void =>{
+    console.log(message)
+}
+
+logMsg('Hello !')
+logMsg(add(2,3))
+
+let subtract = function(c: number, d: number):
+number{
+    return c - d
+}
+
+type mathFunction =(a: number, b: number) => number
+
+let multiply: mathFunction =  function(c,d){
+    return c * d
+}
+
+logMsg(multiply(2,2))
+
+// optional parameters
+const addAll = (a:number, b: number, c?: number):
+number => {
+    if(typeof c !== 'undefined'){
+        return a + b + c
+    } 
+    return a + b   
+}
+
+// default param value
+const sumAll = (a:number, b: number, c: number = 2):
+number => {
+    return a + b + c
+}
+
+logMsg(addAll(2,3,2))
+logMsg(addAll(2,3))
+logMsg(sumAll(2,3))
+
+// Rest Parameters
+const total = (...nums: number[]): number =>{
+    return nums.reduce((prev,curr) => prev + curr)
+}
+
+logMsg(total(1,2,3,4))
+
+// custom type guard
+const isNumber = (value: any): boolean => {
+    return typeof value === 'number'
+    ? true : false
+}
