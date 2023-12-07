04915cd2c2edd4d3895cda10553c33cc0ac6f6fb
Artur Rezmer <a.rezmer@sellpander.pl>
2023-12-07 23:53:20 +0100

    Added firsts events for React

--- React/01tut/src/Content.js
+++ React/01tut/src/Content.js
@@ -5,13 +5,27 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
     }
-    
+
+    const handleClick = () => {
+        console.log('You clicked it');
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`);
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
+
     return (
         <main>
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={() => handleClick2('Dave')}>Click It</button>
+            <button onClick={(e) => handleClick3(e)}>Click It</button>
         </main>
     )
 }
